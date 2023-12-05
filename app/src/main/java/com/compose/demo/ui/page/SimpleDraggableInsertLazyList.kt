27f10f3e36c66bc05da7602ae02a158ddd4422a2
extiovasd106 <xiu.yu.ext@desaysv.com>
2023-12-05 11:15:49 +0800

    commit

--- app/src/main/java/com/compose/demo/ui/page/SimpleDraggableInsertLazyList.kt
+++ app/src/main/java/com/compose/demo/ui/page/SimpleDraggableInsertLazyList.kt
@@ -1,20 +1,16 @@
 package com.compose.demo.ui.page
 
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
@@ -25,17 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.compose.demo.R
-import com.compose.demo.layout.DraggableLazyColumn
 import com.compose.demo.layout.DraggableInsertLazyColumn
-import com.compose.demo.layout.DraggableLazyGrid
 import com.compose.demo.layout.DraggableInsertLazyGrid
-import com.compose.demo.layout.DraggableLazyStaggeredGrid
 import com.google.accompanist.coil.rememberCoilPainter
 import kotlinx.coroutines.launch
 
