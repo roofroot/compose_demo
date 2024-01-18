82af9b097b57373d88822fae8fbf2ca593adb421
coco <565749553@qq.com>
2024-01-18 18:04:59 +0800

    commit

--- app/src/main/java/com/compose/demo/ui/page/SimpleBlurModifier.kt
+++ app/src/main/java/com/compose/demo/ui/page/SimpleBlurModifier.kt
@@ -17,8 +17,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
